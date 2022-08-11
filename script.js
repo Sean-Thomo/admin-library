d9d206f8648d8a27212dbc938604ae904fa02cbc
siphesihle-thomo <seansthomo@gmail.com>
2022-08-11 17:14:23 +0200

    Restyle form and add display toggle class

--- script.js
+++ script.js
@@ -4,6 +4,8 @@ const bookTitle = document.querySelector('.title');
 const bookAuthor = document.querySelector('.author');
 const bookPages = document.querySelector('.pages');
 const bookStatus = document.querySelector('.status');
+const addBook = document.querySelector('.add-book-btn');
+const form = document.querySelector('.form');
 
 let myLibrary = [
   {
@@ -21,8 +23,17 @@ let myLibrary = [
     author: 'J.R.R. Tolkien',
     pages: '295 pages',
     status: 'not ready yet'
+  },{
+    title: 'Title 4',
+    author: 'J.R.R. Tolkien',
+    pages: '295 pages',
+    status: 'not ready yet'
   },
 ]
+
+addBook.addEventListener('click', () => {
+  form.classList.toggle('show');
+})
 
 function Book(title, author, pages, status) {
   this.title = title;
@@ -31,7 +42,7 @@ function Book(title, author, pages, status) {
   this.status = status;
 }
 
-// Loop through array of objects and populate card elemnts
+// Loop through array of objects and populate card elements
 function createCards(array) {
   array.forEach(result => {
     // Construct card 
@@ -54,14 +65,21 @@ function createCards(array) {
   });
 }
 
+function getBookInfo() {
+  const title = document.getElementById('title');
+  const author = document.getElementById('author');
+  const pages = document.getElementById('pages');
+  const status = document.getElementById('status');
+  let book = { title, author, pages, status }
+  return book;
+}
+
 createCards(myLibrary)
 
-Book.prototype.info = function () {
-  return (`${title} by ${author}, ${pages}, ${status}`);
+function addBookToLibrary(e) {
+  e.preventDefault();
+  const newBook = new Book(getBookInfo())
+  myLibrary.push(newBook);
+  return newBook;
 }
-
-function addBookToLibrary() {
-  
-}
-
-bookTitle.textContent = myLibrary[0].title;
+console.log(addBookToLibrary());
